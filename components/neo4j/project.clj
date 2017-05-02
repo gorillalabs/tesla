656a3a0575defccd36ba86224913c19f31a9f4ab
Dr. Christian Betz <cb.betz@gmail.com>
2017-05-02 22:38:31 +0200

    updated even more dependencies

--- components/neo4j/project.clj
+++ components/neo4j/project.clj
@@ -4,8 +4,8 @@
             :description "A neo4j tesla-component"
 
             :dependencies [[gorillalabs.tesla/core :version]
-                           [com.taoensso/timbre "4.6.0"]
+                           [com.taoensso/timbre "4.10.0"]
                            [gorillalabs/neo4j-clj "0.3.1-SNAPSHOT"]
-                           [org.neo4j/neo4j-cypher "3.1.3"]
+                           [org.neo4j/neo4j-cypher "3.1.4"]
                            ])
 
