8ff98d7f3aebc1799ea06b0861f8693899ef309a
Max Lorenz <ml@cypp.hamburg>
2017-06-26 14:29:17 +0200

    Bumped neo4j version

--- components/neo4j/project.clj
+++ components/neo4j/project.clj
@@ -1,11 +1,9 @@
 (defproject gorillalabs.tesla/neo4j "0.4.55-SNAPSHOT"
-            :plugins [[lein-modules "0.3.11"]]
-            :modules {:parent "../.."}
-            :description "A neo4j tesla-component"
+  :plugins [[lein-modules "0.3.11"]]
+  :modules {:parent "../.."}
+  :description "A neo4j tesla-component"
 
-            :dependencies [[gorillalabs.tesla/core :version]
-                           [com.taoensso/timbre "4.6.0"]
-                           [gorillalabs/neo4j-clj "0.3.1-SNAPSHOT"]
-                           [org.neo4j/neo4j-cypher "3.1.3"]
-                           ])
+  :dependencies [[gorillalabs.tesla/core :version]
+                 [com.taoensso/timbre "4.6.0"]
+                 [gorillalabs/neo4j-clj "0.3.3-SNAPSHOT"]])
 
--- components/neo4j/src/gorillalabs/tesla/component/neo4j.clj
+++ components/neo4j/src/gorillalabs/tesla/component/neo4j.clj
@@ -1,26 +1,23 @@
 (ns gorillalabs.tesla.component.neo4j
-    (:require [neo4j-clj.core :as neo4j]
-      [clojure.tools.logging :as log]
-      [gorillalabs.tesla.component.configuration :as config]
-      [mount.core :as mnt]))
+  (:require [neo4j-clj.core :as neo4j]
+            [clojure.tools.logging :as log]
+            [gorillalabs.tesla.component.configuration :as config]
+            [mount.core :as mnt]))
 
 (defn- start []
-       (let [url (config/config config/configuration [:neo4j :url])
-             user (config/config config/configuration [:neo4j :user])
-             password (config/config config/configuration [:neo4j :password])]
-            (log/infof "-> starting neo4j (connecting to url %s as %s)"
-                       url
-                       user)
-            (neo4j/create-connection url user password)))
+  (let [url      (config/config config/configuration [:neo4j :url])
+        user     (config/config config/configuration [:neo4j :user])
+        password (config/config config/configuration [:neo4j :password])]
+    (log/infof "-> starting neo4j (connecting to url %s as %s)" url user)
+    (neo4j/connect url user password)))
 
 (defn- stop [graph]
-       (if graph
-         (do
-           (log/info "<- stopping neo4j component")
-           (.close graph))
-         (log/info "<- neo4j component not running")))
+  (if graph
+    (do
+      (log/info "<- stopping neo4j component")
+      (neo4j/disconnect graph))
+    (log/info "<- neo4j component not running")))
 
 (mnt/defstate graph
-              :start (start)
-              :stop (stop graph))
-
+  :start (start)
+  :stop (stop graph))
