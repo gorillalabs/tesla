62e80fa5855ce3fb7efd3fa6088bd9aa4ea622b7
Max Lorenz <ml@cypp.hamburg>
2017-07-20 16:50:22 +0200

    Version bump

--- components/neo4j/project.clj
+++ components/neo4j/project.clj
@@ -1,9 +1,9 @@
-(defproject gorillalabs.tesla/neo4j "0.4.55-SNAPSHOT"
+(defproject gorillalabs.tesla/neo4j "0.4.56-SNAPSHOT"
   :plugins [[lein-modules "0.3.11"]]
   :modules {:parent "../.."}
   :description "A neo4j tesla-component"
 
   :dependencies [[gorillalabs.tesla/core :version]
                  [com.taoensso/timbre "4.6.0"]
-                 [gorillalabs/neo4j-clj "0.3.3-SNAPSHOT"]])
+                 [gorillalabs/neo4j-clj "0.3.2"]])
 
