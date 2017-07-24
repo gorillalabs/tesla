c0be561acaee2e35f933530c84ca0fde0fce0869
Max Lorenz <ml@cypp.hamburg>
2017-07-24 09:35:34 +0200

    Version 0.5.3

--- components/sente/project.clj
+++ components/sente/project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs.tesla/sente "0.5.3-SNAPSHOT"
+(defproject gorillalabs.tesla/sente "0.5.3"
             :plugins [[lein-modules "0.3.11"]]
             :modules {:parent "../.."}
             :description "sente websocket component"
