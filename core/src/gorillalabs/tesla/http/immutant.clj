343130d85a84706c2c04d759eff65e8328373581
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-17 10:01:31 +0100

    fixed host option
    When ssl is enabled, the host needs to be inside the options generated,
    not "just" assoc'ed. Without this change, the :bind/host option was
    ignored and the server only listened to 127.0.0.1.

--- core/src/gorillalabs/tesla/http/immutant.clj
+++ core/src/gorillalabs/tesla/http/immutant.clj
@@ -1,31 +1,32 @@
 (ns gorillalabs.tesla.http.immutant
-    (:require [immutant.web :as web]
-      [clojure.tools.logging :as log]
-      [gorillalabs.tesla.component.configuration :as config]))
+  (:require [immutant.web :as web]
+            [clojure.tools.logging :as log]
+            [gorillalabs.tesla.component.configuration :as config]))
 
 (def default-port 3000)
 
 (defn server-config [config]
-      (-> (if (get-in config [:immutant.web :keystore])
-            (immutant.web.undertow/options
-              :ssl-port (get-in config [:immutant.web :ssl-port] default-port)
-              :keystore (get-in config [:immutant.web :keystore])
-              :key-password (get-in config [:immutant.web :key-password]))
-            {})
-          (assoc :virtual-host (get-in config [:immutant.web :virtual-host])
-                 :host (get-in config [:immutant.web :bind] "0.0.0.0"))))
+  (-> (if (get-in config [:immutant.web :keystore])
+        (immutant.web.undertow/options
+          :host (get-in config [:immutant.web :bind] "0.0.0.0")
+          :ssl-port (get-in config [:immutant.web :ssl-port] default-port)
+          :keystore (get-in config [:immutant.web :keystore])
+          :key-password (get-in config [:immutant.web :key-password]))
+        {})
+      (assoc :virtual-host (get-in config [:immutant.web :virtual-host])
+             :host (get-in config [:immutant.web :bind] "0.0.0.0"))))
 
 
 (defn start [handler]
-      (log/info "-> starting immutant.web")
-      (let [server-config (server-config config/configuration)]
-           (log/info "Starting immutant.web")
-           (immutant.web/run handler server-config)))
+  (log/info "-> starting immutant.web")
+  (let [server-config (server-config config/configuration)]
+    (log/info "Starting immutant.web with config " server-config)
+    (immutant.web/run handler server-config)))
 
 (defn stop [server]
-      (let [timeout (config/config config/configuration [:httpkit-timeout] 100)]
-           (if server
-             (do
-               (log/info "<- stopping immutant.web")
-               (immutant.web/stop server))
-             (log/info "<- stopping immutant.web"))))
+  (let [timeout (config/config config/configuration [:httpkit-timeout] 100)]
+    (if server
+      (do
+        (log/info "<- stopping immutant.web")
+        (immutant.web/stop server))
+      (log/info "<- stopping immutant.web"))))
